3ee378986441c48d81029de7a5424b1ceb5e94e8
Jacob Deinum <jdeinum@nullspaces.org>
2026-07-06 16:49:50 -0600

    fix: logs to stderr

--- crates/cargo-notch/src/main.rs
+++ crates/cargo-notch/src/main.rs
@@ -1,7 +1,7 @@
 use cargo_notch::run;
 use tracing::error;
 pub fn main() {
-    tracing_subscriber::fmt::init();
+    tracing_subscriber::fmt().with_writer(std::io::stderr).init();
     match run() {
         Ok(()) => {}
         Err(e) => error!("Error running the build tool: {e:?}"),
