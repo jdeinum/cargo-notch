ee05bca727de4d277940b3caa6abf582e1e41355
Jacob Deinum <jdeinum@nullspaces.org>
2026-07-20 21:23:22 -0600

    fix: env filter for logs

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
 use cargo_notch::run;
 use tracing::error;
+use tracing_subscriber::EnvFilter;
 pub fn main() {
     tracing_subscriber::fmt()
         .with_writer(std::io::stderr)
+        .with_env_filter(EnvFilter::try_from_default_env().unwrap_or_else(|_| "info".into()))
         .init();
     match run() {
         Ok(()) => {}
